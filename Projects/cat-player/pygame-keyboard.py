1af72b5fba1c33e5848ac9a838ba92c5e8a6398e
Louisredstone <zzy2900920898@163.com>
2021-06-19 22:07:17 +0800

    2021/06/19 周六

---
+++ Projects/cat-player/pygame-keyboard.py
@@ -0,0 +1,27 @@
+import pygame
+import tkinter
+from tkinter import ttk
+pygame.mixer.init()
+
+track = pygame.mixer.music.load(r"cat-cry-short.mp3")
+
+ 
+ 
+def xFunc1(event):
+    print(f"事件触发键盘输入:{event.char},对应的ASCII码:{event.keycode}")
+    # if event.char==" ":
+    pygame.mixer.music.play()
+ 
+win = tkinter.Tk()
+win.title("Kahn Software v1")    # #窗口标题
+win.geometry("600x500+200+20")   # #窗口位置500后面是字母x
+'''
+响应所有事件(键盘)
+<Key>   所有键盘按键会触发
+'''
+xLabel = tkinter.Label(win, text="KAHN Hello world")
+xLabel.focus_set()
+xLabel.pack()
+xLabel.bind("<Key>", xFunc1)
+ 
+win.mainloop()   # #窗口持久化
